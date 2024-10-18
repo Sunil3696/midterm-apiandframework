fc8bbd9d3e02401a6509c0065f6dac1e6f0e927d
sunil3696 <sunilstark3696@gmail.com>
2024-10-18 12:30:38 -0400

    added comments on userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,19 @@
+/**
+ * File: userRoutes.js
+ * Author: Sunil Balami
+ * StudentID: 200578456
+ * Date: 2024-10-18
+ * Description: Routes to handle different request related to user.
+ */
+
 const express = require("express");
+// Creating a new router instance
 const router = express.Router();
 const { getAllUsers , getUserById, getUserByLocation} = require("../controller/userController");
 
-router.get('/users',getAllUsers);
+
+//all routes goes here
+router.get('/users',getAllUsers);  
 router.get('/users/:id', getUserById);
 router.get('/users/location/:location', getUserByLocation);
 
