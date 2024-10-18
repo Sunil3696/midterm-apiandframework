03e71ea702c94ce5a2159294be73650ff7b7441b
sunil3696 <sunilstark3696@gmail.com>
2024-10-18 11:02:03 -0400

    connected route with controller for getuserbyid

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUsers } = require("../controller/userController");
+const { getAllUsers , getUserById} = require("../controller/userController");
 
 router.get('/users',getAllUsers);
+router.get('/users/:id', getUserById);
 
 module.exports = router; //exporting router
