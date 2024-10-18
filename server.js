9a43379899df8d0d0ee496201ad3a43f63bf4888
sunil3696 <sunilstark3696@gmail.com>
2024-10-18 10:06:34 -0400

    wrote essential code to start the server

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+const MongodbURI = "mongodb+srv://sunil:sunil123@cluster0.df1iq.mongodb.net/";
+
+//parshing JSON requests
+app.use(express.json());
+
+
+//Starting the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
