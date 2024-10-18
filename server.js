3c394ab006c2e5dc8db0054f0dfac1c480059c0e
sunil3696 <sunilstark3696@gmail.com>
2024-10-18 11:00:37 -0400

    started working on getUserById

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./src/routes/userRoutes");
-const fs = require('fs');
+const fs = require("fs");
 const User = require("./src/models/userModel");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const MongodbURI = "mongodb+srv://suniluser:sunil123@cluster0.j50og.mongodb.net/";
+const MongodbURI =
+  "mongodb+srv://suniluser:sunil123@cluster0.j50og.mongodb.net/";
 
 mongoose
   .connect(MongodbURI, {
@@ -19,28 +20,26 @@ mongoose
 //parshing JSON requests
 app.use(express.json());
 
-const data = JSON.parse(fs.readFileSync('./users.json', 'utf-8'));
-
+const data = JSON.parse(fs.readFileSync("./users.json", "utf-8"));
+//import function to seed the database
 const importUsers = async (req, res) => {
-    try {
-        const count = await User.countDocuments();
-        if (count === 0) {
-            await User.create(data);
-            console.log('Data successfully imported to MongoDB');
-            res.status(200).send('Data successfully imported');
-        } else {
-            console.log('Data already in the database');
-            res.status(200).send('Data already in the database');
-        }
-    } catch (e) {
-        console.error('Error importing data', e);
-        res.status(500).send('Error importing data');
+  try {
+    const count = await User.countDocuments();
+    if (count === 0) {
+      await User.create(data);
+      console.log("Data successfully imported to MongoDB");
+      res.status(200).send("Data successfully imported");
+    } else {
+      console.log("Data already in the database");
+      res.status(200).send("Data already in the database");
     }
+  } catch (e) {
+    console.error("Error importing data", e);
+    res.status(500).send("Error importing data");
+  }
 };
 
-app.get('/import-users', importUsers);
-
-
+app.get("/import-users", importUsers);
 
 app.use("/", userRoutes); //Base URL for recipe
 
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,27 +1,30 @@
 const User = require("../models/userModel");
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 // const testfunc = async (req, res) => {
 //     res.send("you are workig fine")
 // }
 
 const getAllUsers = async (req, res) => {
-//   res.send("getall")
-try {
+  //   res.send("getall")
+  try {
     const users = await User.find();
 
-    if(users){
-       res.status(200).send(users);
-    }else {
-        res.status(404).json({error : "No data found"})
+    if (users) {
+      res.status(200).send(users);
+    } else {
+      res.status(404).json({ error: "No data found" });
     }
-}
-catch (error) {
+  } catch (error) {
     console.log("error:", error);
-    res.status(500).json({message: "error orccured while getting users", error})
-}
-
-
+    res
+      .status(500)
+      .json({ message: "error orccured while getting users", error });
+  }
 };
 
-module.exports = { getAllUsers };
+const getUserById = async (req, res) => {
+    res.send("I am get user by id")
+}
+
+module.exports = { getAllUsers ,getUserById};
