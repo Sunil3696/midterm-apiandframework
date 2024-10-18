a657e3998ccf5dce7f5a42c1af2f1a5811799596
sunil3696 <sunilstark3696@gmail.com>
2024-10-18 10:20:52 -0400

    started adding testing controller to check if the code is working or not

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,3 +1,8 @@
 const User = require('../models/userModel');
 const fs = require('fs');
 
+const testfunc = async (req, res) => {
+    res.send("you are workig fine")
+}
+
+module.exports = { testfunc };
